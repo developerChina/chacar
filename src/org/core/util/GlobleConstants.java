bc56f6187e1e580dbcf15204812acb8304083639
sunjunhu <findme1110@sina.com>
2017-10-21 15:51:10 +0800

    Signed-off-by: sunjunhu <findme1110@sina.com>

--- src/org/core/util/GlobleConstants.java
+++ src/org/core/util/GlobleConstants.java
@@ -1,18 +1,21 @@
 package org.core.util;
 
 public class GlobleConstants {
-	
+
 	// 数据库表常量
 	public static final String EXAMPLETABLE = "example";
-		
+
+	// 数据库表常量
+	public static final String USERTABLE = "user_inf";
+	public static final String DEPTTABLE = "dept_inf";
+	public static final String JOBTABLE = "job_inf";
+	public static final String EMPLOYEETABLE = "employee_inf";
+
 	// 登录
 	public static final String LOGIN = "loginForm";
 	// 用户的session对象
 	public static final String USER_SESSION = "user_session";
-	
+
 	// 默认每页4条数据
 	public static int PAGE_DEFAULT_SIZE = 4;
-	
-	
-	
 }
