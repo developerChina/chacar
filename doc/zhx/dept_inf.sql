21a16d8dc2689d8c36802c33fcaca4a5a98a14ce
zhanghaixing <suxinghai1988@163.com>
2017-11-03 01:20:57 +0800

    Signed-off-by: zhanghaixing <suxinghai1988@163.com>

---
+++ doc/zhx/dept_inf.sql
@@ -0,0 +1,7 @@
+INSERT INTO `dept_inf` VALUES (1, '技术部', NULL, '技术部', NULL);
+INSERT INTO `dept_inf` VALUES (2, '运营部', NULL, '运营部', NULL);
+INSERT INTO `dept_inf` VALUES (3, '财务部', NULL, '财务部', NULL);
+INSERT INTO `dept_inf` VALUES (5, '总公办', NULL, '总公办', NULL);
+INSERT INTO `dept_inf` VALUES (6, '市场部', NULL, '市场部', NULL);
+INSERT INTO `dept_inf` VALUES (7, '教学部', 1, '教学部', NULL);
+INSERT INTO `dept_inf` VALUES (24, '技术1部', 1, '技术1部', NULL);
---
+++ doc/zhx/resource_info.sql
@@ -0,0 +1,4 @@
+INSERT INTO `resource_info` VALUES (1, '系统管理', NULL, NULL, NULL, '2017-11-1', NULL);
+INSERT INTO `resource_info` VALUES (2, '用户管理', 1, '系统管理', '/user/selectUser', '2017-11-1', NULL);
+INSERT INTO `resource_info` VALUES (3, '资源管理', 1, '系统管理', '/resource/resourcesAck', NULL, NULL);
+INSERT INTO `resource_info` VALUES (7, '员工系统', NULL, NULL, NULL, '2017-11-3', NULL);
