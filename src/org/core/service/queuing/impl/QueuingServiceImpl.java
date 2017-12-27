bf25a06c8481ba6f032930249b799f24e61268fe
weiwei <17614742199@163.com>
2017-12-27 15:49:14 +0800

    Signed-off-by: weiwei <17614742199@163.com>

--- src/org/core/service/queuing/impl/QueuingServiceImpl.java
+++ src/org/core/service/queuing/impl/QueuingServiceImpl.java
@@ -204,7 +204,7 @@ public class QueuingServiceImpl implements QueuingService {
 			System.out.println(Hparts.getGoout_time());
 			long hour1=between%(24*3600)/3600;
 			long minute1=between%3600/60;
-			long second1=between%60/60;
+			long second1=between%60;
 			Hparts.setReduce(""+hour1+"小时"+minute1+"分"+second1+"秒");
 		}
 		return pageListH;
